e292071fced972784ce3dfe0f7a24fb6e023e0ea
Siam-Parvez <tanmoyparvez23@gmail.com>
2021-10-04 03:10:31 +0600

    services added

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,41 +1,35 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import "./Courses.css"
 
 const Courses = () => {
+    const [courses, setCourse] = useState([]);
+    useEffect(() => {
+        fetch('/fakeData.json')
+            .then(res => res.json())
+            .then(data => setCourse(data))
+    }, [])
     return (
         <div className="course-container p-5 my-5">
             <div className="container text-center text-light">
-                <h5>Browse Categories</h5>
+                <h5>Browse Categories {courses.length}</h5>
                 <h1>Popular topics to learn</h1>
                 <div className="row my-5">
-                    <div className="col-md-3">
-                        <div className="card bg-dark">
-                            <img src="" alt="" />
-                            <h3>Computer Science</h3>
-                            <h5>Price: $100</h5>
-                        </div>
-                    </div>
-                    <div className="col-md-3">
-                        <div className="card bg-dark">
-                            <img src="" alt="" />
-                            <h3>Computer Science</h3>
-                            <h5>Price: $100</h5>
-                        </div>
-                    </div>
-                    <div className="col-md-3">
-                        <div className="card bg-dark">
-                            <img src="" alt="" />
-                            <h3>Computer Science</h3>
-                            <h5>Price: $100</h5>
-                        </div>
-                    </div>
-                    <div className="col-md-3">
-                        <div className="card bg-dark">
-                            <img src="" alt="" />
-                            <h3>Computer Science</h3>
-                            <h5>Price: $100</h5>
-                        </div>
-                    </div>
+                    {
+                        courses.slice(0, courses.length - 5).map(course => <div className="col-md-3">
+                            <div className="card bg-dark">
+                                <div className="bg-light card-logo">
+                                    <img src={course.img} alt="" />
+                                </div>
+                                {/* <div>
+                                    <h3>{course.title}</h3>
+                                    <h5>{course.subTitle}</h5>
+                                    <h5>Price: ${course.price}</h5>
+                                </div> */}
+                            </div>
+                        </div>)
+                    }
 
                 </div>
             </div>
